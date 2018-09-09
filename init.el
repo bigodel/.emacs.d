c5dc48d90b3ca8a6fb7cd156f6a0363ea74f92eb
João Pedro <jpedrodeamorim@gmail.com>
2018-09-09 16:11:00 -0300

    Enabled menu-bar-mode (it it actually useful :P)

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 
   (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
   (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-  (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+  ;; (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
   (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
 
 (defvar dotemacs-cache-directory (concat user-emacs-directory ".cache/")
