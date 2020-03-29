092d552108a27fddc7e51ee7020ccdc44a323bdd
João Pedro <jpedrodeamorim@gmail.com>
2020-03-29 00:45:06 -0300

    wrap lsp config in a defun so it isn't installed

--- config/config-lsp.el
+++ config/config-lsp.el
@@ -6,25 +6,43 @@
 
 ;;; Code:
 ;;; install lsp-mode
-(require-package 'lsp-mode)
-;; helper packges
-(after 'lsp-mode
+(defun lsp-start ()
+  "Start `lsp-mode' with my configurations.
+
+This is wrapped in a function because I don't want to install
+`lsp-mode' and all the other helper modes and set all the
+configuration if I'm not going to use `lsp-mode'. So, in each
+configuration file that I want to use lsp, instead of using the
+`lsp' function, I use my `lsp-start'."
+  (interactive)
+
+  ;; install the necessary packages
+  (require-package 'lsp-mode)
   (require-package 'lsp-ui)
   (require-package 'dap-mode)        ; has lsp-mode and treemacs as dependencies
-  ;; ivy wrapper
   (after 'ivy
-    (require-package 'lsp-ivy))
-  ;; treemacs wrapper
+    (require-package 'lsp-ivy))         ; TODO: see the necessity of this
   (after 'treemacs
-    (require-package 'lsp-treemacs)))
+    (require-package 'lsp-treemacs))    ; TODO: see the necessity of this
 
-(setvar 'lsp-keymap-prefix "C-l")        ; change the default prefix for lsp
-(setvar 'lsp-auto-configure t)           ; will configure company, flycheck, ...
-(setvar 'lsp-log-io t)                   ; log msgs from the ls in *lsp-log*
-(setvar 'lsp-session-file                ; where to store the session file
-        (concat dotemacs-cache-directory ".lsp-session-v1"))
-(setvar 'lsp-enable-semantic-highlighting t) ; experimental semantic highlight
+  ;; variables
+  (setvar 'lsp-keymap-prefix "C-l")        ; change the default prefix for lsp
+  (setvar 'lsp-auto-configure t)        ; will configure company, flycheck, ...
+  (when (fboundp 'flycheck-mode)
+    (setvar 'lsp-prefer-flymake nil))     ; force flycheck
+  (setvar 'lsp-log-io t)                   ; log msgs from the ls in *lsp-log*
+  (setvar 'lsp-session-file                ; where to store the session file
+          (concat dotemacs-cache-directory ".lsp-session-v1"))
+  (setvar 'lsp-enable-semantic-highlighting t) ; experimental semantic highlight
+  (setvar 'lsp-diagnostics-modeline-scope :project) ; modeline show project err
 
+  ;; start lsp
+  (lsp)
+  (lsp-treemacs-sync-mode t))            ; TODO: investigate what is this
+
+(after 'lsp-mode
+  ;; see error statistics in modeline TODO: test this!
+  (add-hook 'lsp-managed-mode-hook #'lsp-diagnostics-modeline))
 
 ;; enable which key integration
 (after 'which-key
