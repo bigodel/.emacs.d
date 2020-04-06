cfe3dd64504db6398b1725af0bc93cf7e77fc496
João Pedro <jpedrodeamorim@gmail.com>
2020-04-06 03:49:25 -0300

    bindings for dart, org and new snippets

---
+++ bindings/modes/bindings-dart.el
@@ -0,0 +1,20 @@
+;;; bindings-dart.el --- Dart mode bindings
+
+;; Author: João Pedro de Amorim Paula <maybe_add_email@later>
+
+;;; Commentary:
+
+;;; Code:
+(after 'dart-mode
+  (when (package-installed-p 'flutter)
+    (bindings-define-keys dart-mode-map
+      ((kbd "C-M-x") #'flutter-run-or-hot-reload)
+      ([remap compile] #'flutter-run)
+      ([remap recompile] #'flutter-run-or-hot-reload))
+
+    ;; TODO: fix flutter bindings
+    ;; (bindings-define-keys flutter-mode-map)
+    ))
+
+(provide 'config-bindings-dart)
+;;; bindings-dart.el ends here
---
+++ bindings/modes/bindings-org.el
@@ -0,0 +1,27 @@
+;;; bindings-pg.el --- Org mode bindings
+
+;; Author: João Pedro de Amorim Paula <maybe_add_email@later>
+
+;;; Commentary:
+
+;;; Code:
+(after 'org
+  (bindings-define-keys (current-global-map)
+    ((kbd "C-c o c") #'org-capture)
+    ((kbd "C-c o a") #'org-agenda)
+    ((kbd "C-c o l") #'org-store-link)
+    )
+
+  (after 'evil
+    (evil-define-key '(normal visual motion) 'global
+      "gC" #'org-capture
+      "ga" #'org-agenda
+      "goc" #'org-capture
+      "goa" #'org-agenda
+      "gol" #'org-store-link)
+
+    (evil-define-key '(normal visual) org-mode-map
+      "gt" #'org-todo)))
+
+(provide 'config-bindings-org)
+;;; bindings-org.el ends here
