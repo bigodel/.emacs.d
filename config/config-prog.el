fa54a94b093f6189a0980a2bef7746c221f177d0
João Pedro <jpedrodeamorim@gmail.com>
2020-04-06 04:05:02 -0300

    folding, rainbow-delimiters and highlight indent

--- config/config-prog.el
+++ config/config-prog.el
@@ -20,11 +20,30 @@ tabs."
         (tab-count (how-many "^\t" (point-min) (point-max))))
     (when (> tab-count space-count)
       (setvar 'indent-tabs-mode t 'local))))
-
+;; always infer-indentation on `prog-mode' files
 (add-hook 'prog-mode-hook #'infer-indentation-style)
 
-;;; code folding
+;;; enable tabs instead of spaces in makefiles
+;; TODO: move this to config-makefile.el maybe???
+(add-hook 'makefile-mode-hook (lambda () (setvar 'indent-tabs-mode t 'local)))
+
+;;; folding
 (add-hook 'prog-mode-hook #'hs-minor-mode)
+
+(defun hs-fold-overlay (ov)
+  "Function to be set up the overlay OV to be displayed by `hs-minor-mode'."
+  (when (eq 'code (overlay-get ov 'hs))
+    (let ((col (save-excursion
+                 (move-end-of-line 0)
+                 (current-column)))
+          (count (count-lines (overlay-start ov) (overlay-end ov))))
+      (overlay-put ov 'after-string
+                   (format "%s [ %d lines ] "
+                           (make-string (- (window-width) col 32)
+                                        (string-to-char " "))
+                           count)))))
+
+(setvar 'hs-set-up-overlay #'hs-fold-overlay)
 
 ;;; bunch of modes that don't come with emacs
 (lazy-major-mode "\\.toml\\'" toml-mode)
@@ -47,13 +66,21 @@ tabs."
     java-mode-hook
     python-mode-hook
     makefile-mode-hook
-    dart-mode-hook
     proof-mode-hook)
   "Hooks for major modes to activate `aggressive-indent-mode'.")
 
 (require-package 'aggressive-indent)
 (dolist (hook dotemacs-misc-aggressive-indent-hooks)
   (add-hook hook #'aggressive-indent-mode))
+
+;;; rainbow delimiters
+(require-package 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;;; highlight indentation
+(require-package 'highlight-indent-guides)
+(setvar 'highlight-indent-guides-method 'character) ; display a 1px char
+(add-hook 'prog-mode-hook #'highlight-indent-guides-mode)
 
 (provide 'config-prog.el)
 ;;; config-prog.el ends here
