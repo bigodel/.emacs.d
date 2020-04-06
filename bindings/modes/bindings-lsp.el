ff08769cab61b6b1f57c1562028322c5f798060f
João Pedro <jpedrodeamorim@gmail.com>
2020-04-06 03:58:41 -0300

    added doc bindings, evil bindings and lsp-treemacs

--- bindings/modes/bindings-lsp.el
+++ bindings/modes/bindings-lsp.el
@@ -18,23 +18,33 @@
       ;; ([remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
       ;; ([remap xref-find-references] #'lsp-ui-peek-find-references)
       ((kbd "M-l <tab>") #'lsp-ui-doc-focus-frame) ; gui
-      ((kbd "M-l TAB") #'lsp-ui-doc-focus-frame)))) ; terminal
+      ((kbd "M-l tab") #'lsp-ui-doc-focus-frame) ; gui
+      ((kbd "M-l TAB") #'lsp-ui-doc-focus-frame))) ; terminal
 
-(after [lsp-mode evil]
-  (evil-define-key '(normal visual) lsp-mode-map
-    "ga" #'lsp-execute-code-action
-    "gR" #'lsp-rename)
+  (after 'lsp-treemacs
+    (bindings-define-keys lsp-mode-map
+      ([remap flycheck-list-errors] #'lsp-treemacs-errors-list)
+      ([remap lsp-find-references] #'lsp-treemacs-references)))
 
-  (after 'lsp-ui
+  (after 'evil
     (evil-define-key '(normal visual) lsp-mode-map
-      "gD" #'lsp-ui-doc-glance
-      "g?" #'lsp-ui-doc-glance)
+      "K" #'lsp-describe-thing-at-point
+      "ga" #'lsp-execute-code-action
+      "gr" #'lsp-rename)
 
-    (evil-define-key '(normal visual) lsp-ui-imenu-mode-map
-      "q" #'lsp-ui-imenu--kill)
+    ;; TODO: add lsp-ui-peek-find-definitions
+    (after 'lsp-ui
+      (evil-define-key '(normal visual) lsp-mode-map
+        "ga" #'lsp-execute-code-action
+        "gR" #'lsp-rename
+        "gD" #'lsp-ui-doc-glance
+        "g?" #'lsp-ui-doc-glance)
 
-    (evil-define-key '(normal visual) lsp-ui-doc-frame-mode-map
-      "q" #'lsp-ui-doc-unfocus-frame)))
+      (evil-define-key '(normal visual) lsp-ui-imenu-mode-map
+        "q" #'lsp-ui-imenu--kill)
 
-(provide 'bindings-lsp)
+      (evil-define-key '(normal visual) lsp-ui-doc-frame-mode-map
+        "q" #'lsp-ui-doc-unfocus-frame))))
+
+(provide 'config-bindings-lsp)
 ;;; bindings-lsp.el ends here
