f596d4b0f5c6427aadcd62bfc41074a430fbd9af
João Pedro <jpedrodeamorim@gmail.com>
2020-06-22 23:05:18 -0300

    wrapped the config in a function to call after-init

--- config/config-spelling.el
+++ config/config-spelling.el
@@ -5,31 +5,36 @@
 ;;; Commentary:
 
 ;;; Code:
-;; only enable spelling and go through its configurations if one of the
-;; programs is available
-(when (or (executable-find "aspell")
-          (executable-find "ispell")
-          (executable-find "hunspell"))
-  (cond
-   ;; configuration for hunspell
-   ((executable-find "hunspell")
-    (setvar 'ispell-program-name (executable-find "hunspell"))
-    ;; default dictionary (overrided by `ispell-local-dictionary')
-    (setvar 'ispell-dictionary "en_US,pt_BR")
-    (after 'ispell
-      ;; enable the use of multiple dictionaries with hunspell
-      ;; `ispell-set-spellchecker-params' has to be called
-      ;; before `ispell-hunspell-add-multi-dic' will work
-      (ispell-set-spellchecker-params)
-      (ispell-hunspell-add-multi-dic "en_US,pt_BR")))
-   ;; configuration for aspell
-   ((executable-find "aspell")
-    (setvar 'ispell-program-name "aspell"))
-   (t
-    (setvar 'ispell-program-name "ispell")))
+(defun spelling-start-ispell ()
+  "Start `ispell' and configure its variables."
+  (interactive)
+  ;; only enable spelling and go through its configurations if one of the
+  ;; programs is available
+  (when (or (executable-find "aspell")
+            (executable-find "ispell")
+            (executable-find "hunspell"))
+    (cond
+     ;; configuration for hunspell
+     ((executable-find "hunspell")
+      (setvar 'ispell-program-name (executable-find "hunspell"))
+      ;; default dictionary (overrided by `ispell-local-dictionary')
+      (setvar 'ispell-dictionary "en_US,pt_BR")
+      (after 'ispell
+        ;; enable the use of multiple dictionaries with hunspell
+        ;; `ispell-set-spellchecker-params' has to be called
+        ;; before `ispell-hunspell-add-multi-dic' will work
+        (ispell-set-spellchecker-params)
+        (ispell-hunspell-add-multi-dic "en_US,pt_BR")))
+     ;; configuration for aspell
+     ((executable-find "aspell")
+      (setvar 'ispell-program-name "aspell"))
+     (t
+      (setvar 'ispell-program-name "ispell")))
 
-  (add-hook 'text-mode-hook #'turn-on-flyspell)
-  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+    (add-hook 'text-mode-hook #'turn-on-flyspell)
+    (add-hook 'prog-mode-hook #'flyspell-prog-mode)))
+
+(add-hook 'after-init-hook #'spelling-start-ispell)
 
 (provide 'config-spelling)
 ;;; config-spelling.el ends here
