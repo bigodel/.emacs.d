1007ad606356d377e90a91fedf720d9b2172662d
João Pedro <jpedrodeamorim@gmail.com>
2020-07-29 11:53:44 -0300

    (failed) attempt at not having abbrevs
    i have also disabled company-coq.

--- config/config-pg.el
+++ config/config-pg.el
@@ -15,40 +15,34 @@
 (setvar 'proof-three-window-mode-policy 'smart)
 (setvar 'proof-splash-enable nil)
 (setvar 'proof-script-fly-past-comments t)
-(setvar 'proof-abbrev-mode-table nil)
 
 ;;; coq variables
 (setvar 'coq-compile-before-require t)
 (setvar 'coq-one-command-per-line nil)
 (setvar 'coq-holes-minor-mode nil)
-;; i hate the default abbrevs, i'll be adding my own
-(setvar 'coq-mode-abbrev-table nil)
+(setvar 'coq-mode-abbrev-table '())
 
-;; coq misc configurations
 (after 'coq-mode
+  ;; coq abbrevs
+  (define-abbrev-table 'coq-mode-abbrev-table '())
   (define-abbrev coq-mode-abbrev-table "ref" "reflexivity.")
   (define-abbrev coq-mode-abbrev-table "ind" "induction")
-  (define-abbrev coq-mode-abbrev-table "sim" "simpl.")
-  ;; always expand abbrev after typing . (coq-terminator-insert)
-  (advice-add 'coq-terminator-insert :before #'expand-abbrev))
+  (define-abbrev coq-mode-abbrev-table "sim" "simpl."))
 
 ;; install `company-coq'
-(after [company coq-mode]
-  (require-package 'company-coq)
-  ;; deactivate some features
-  (setvar 'company-coq-disabled-features
-          '(smart-subscripts            ; self explanatory
-            prettify-symbols            ; self explanatory
-            company-defaults))          ; automatic company config
+;; (after [company coq-mode]
+;;   (require-package 'company-coq)
+;;   ;; deactivate some features
+;;   (setvar 'company-coq-disabled-features
+;;           '(smart-subscripts            ; self explanatory
+;;             prettify-symbols            ; self explanatory
+;;             company-defaults))          ; automatic company config
 
-  ;; activate company-coq
-  (add-hook 'coq-mode-hook #'company-coq-mode))
+;;   ;; activate company-coq
+;;   (add-hook 'coq-mode-hook #'company-coq-mode))
 
 ;;; hooks and misc config
 (after [proof-site undo-tree]
-  ;; always expand abbrev before executing next command interactively
-  (advice-add 'proof-assert-next-command-interactive :before #'expand-abbrev)
-
   ;; this series of functions make it so that undo-tree will sort of behave with
   ;; proof-general. by default, PG disables undo-tree. there is an open issue on
   ;; github where discussion is been had and where i took these functions from:
