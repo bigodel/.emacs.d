ab0e44bfa33953199800ad601893fce2efdd2b53
João Pedro <jpedrodeamorim@gmail.com>
2020-12-17 14:37:31 -0300

    i intend to setup emms

---
+++ config/config-emms.el
@@ -0,0 +1,10 @@
+;;; config-emms.el --- EMMS configuration -*- lexical-bindings: t; -*-
+
+;; Author: João Pedro de Amorim Paula <maybe_add_email@later>
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'config-emms)
+;;; config-emms.el ends here
